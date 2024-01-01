07060f0451afeaf9b0fc6563d633f0cd32180baa
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-01-01 18:08:39 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -1,1 +1,14 @@
+#include "interface.h"
 
+
+void display() 
+{
+printf("\033[1m----------TP2 SFSD----------\n");
+printf("\t\t\t\t\033[1mAllag Ahmed Yacine\n");
+printf("\t\t\t\t\033[1mBOUDRAA MAHDI\n");
+printf("\033[1mCe programme sert a manipuler une simple base de donnee de l'armee algerienne\n");
+printf("\033[1mIl y a un fichier deja cree -- si vous voulez tester notre options sur le\n");
+printf("\033[1mSi vous voulez tester sur un nouver fichier, vous pouvez choisir l'option de creer un nouveau fichier, suivre les etapes et puis choisir une option\n");
+printf("\033[1mMerci de choisir une option: ");
+
+}
