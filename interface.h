335d19df4d41d1728ead9aa4a7dd2d38e5f8633e
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-01-01 18:36:28 +0100

    Update interface.h

--- interface.h
+++ interface.h
@@ -4,6 +4,14 @@
 
 #include "functions.h"
 
+
+void DisplaySearchSoldier();
+void DisplayAddSoldier();
+void isplayDeleteSoldier();
+void DisplayRequeteIntervale();
+void DisplayRequeteIntervale();
+void DisplayPrintFile();
+void DisplayChangeRegion();
 void display() ;
 
 #endif
