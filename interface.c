50ac3a69137d24b06f899222b856218c6de5a77e
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-01-01 20:34:02 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -19,11 +19,11 @@ void DisplaySearchSoldier()
   recherche(filepath,matricule,&trouve,&i,&j,&t) ;
   if (trouve)
   {
-    printf("\033[1mElement Trouve sur le bloc %d, position %d",i,j);
+    printf("\033[1;32mElement Trouve sur le bloc %d, position %d",i,j);
   }
   else 
   {
-    printf("\033[1mElement n'existe pas");
+    printf("\033[1;32mElement n'existe pas");
   }
 
   return ;
@@ -38,7 +38,7 @@ void DisplayAddSoldier()
   
   
   printf("\033[1mEntrez le chemin du fichier:");
-  scanf(" %99[^\n]", filepath);
+  scanf(" %s", filepath);
   printf("\n");
   
   printf("\033[1mEntrez le matricule a inserer:");
@@ -79,33 +79,94 @@ void DisplayAddSoldier()
   printf("\n");
   
   printf("\033[1mEntrez la force armee:");
-  scanf(" %c", e.forceArmee);
+  scanf(" %s", e.forceArmee);
   printf("\n");
   
-  printf("\033[1mEntez la region:");
-  scanf(" %c", e.regionMilitaire);
+  printf("\033[1mEnrtez la region:");
+  scanf(" %s", e.regionMilitaire);
   printf("\n");
-  
+
+  printf("\033[1;32mInsertion EN COURS...\n");
+  insertion(filepath,e) ;
+  printf("\033[1;32mC'est bon vous pouvez verrifier le fichier...");
+  return ;  
 }
-void isplayDeleteSoldier()
+void DisplayDeleteSoldier()
 {
-  //
+  char filepath [50] ;
+  int matricule ;
+  printf("\033[1mle chemin du fichier:");
+  scanf(" %99[^\n]", filepath);
+
+  printf("\033[1mTapez le Matricule a supprimer:");
+  scanf(" %d", &matricule);
+
+  printf("\n");
+  printf("\033[1;32mSuppression EN COURS...\033[0m\n");
+
+  suppression(filepath,matricule) ;
+  printf("\033[1;32mSuppression fini, verifier le fichier...\n");
+  
+  
+  
 }
 void DisplayRequeteIntervale()
 {
-  //
-}
-void DisplayRequeteIntervale()
-{
-  //
+  char filepath [50] ;
+  int matricule1,matricule2 ;
+  printf("\033[1mle chemin du fichier:");
+  scanf(" %99[^\n]", filepath);
+
+  do 
+  {
+    printf("\033[1mTapez le Matricule inferieur:");
+    scanf(" %d", &matricule1);
+    printf("\033[1mTapez le Matricule superieur:");
+    scanf(" %d", &matricule2);
+  } while (matricule1>matricule2)
+    printf("\n") ;
+
+  requeteIntervalle(filepath,matricule1,matricule2) ;
+
+  return ;
+  
 }
 void DisplayPrintFile()
 {
-  //
+  char filepath [50] ;
+  printf("\033[1mle chemin du fichier:");
+  scanf(" %99[^\n]", filepath);
+  printf("\n")
+  printFile(filepath) ;
+  
 }
+
+void DisplayCreateNewFile()
+{
+  char filepath [50] ;
+  int length ;
+  printf("\033[1mle chemin du fichier:");
+  scanf(" %99[^\n]", filepath);
+  do 
+  {
+    printf("\033[1mTapez le nombre de soldat:");
+    scanf(" %d", &length);
+  } while (length<=1000000)
+
+  chargement_init(filepath,length) ;
+  
+}
+
+
 void DisplayChangeRegion()
 {
-  //
+  char filepath [50] ;
+  int i ; 
+  printf("\033[1mle chemin du fichier:");
+  scanf(" %99[^\n]", filepath);
+  printf("\033[1mTapez le numero de la region:");
+  scanf(" %d", &i);
+  modifierRegion(filepath,i) ;   
 }
 
 
@@ -119,6 +180,7 @@ void display()
   keep = true ;
   do 
   {
+    printf("\n") ;
     printf("\033[1mCe programme sert a manipuler une simple base de donnee de l'armee algerienne\n");
     printf("\033[1mIl y a un fichier deja cree -- si vous voulez tester notre options sur le\n");
     printf("\033[1mSi vous voulez tester sur un nouver fichier, vous pouvez choisir l'option de creer un nouveau fichier, suivre les etapes et puis choisir une option\n");
@@ -129,7 +191,8 @@ void display()
     printf("\033[1m3. Supprimer un solday depuis matricule\n");
     printf("\033[1m4. Requete intervalle\n");
     printf("\033[1m5. Afficher tous les soldats\n");
-    printf("\033[1m6. Changer la region militaire d'un soldat utilisons son matricule\n\n\n");
+    printf("\033[1m6. creer un nouveau fichier\n");
+    printf("\033[1m7. Changer la region militaire d'un soldat utilisons son matricule\n\n\n");
     printf("\033[1mTapez votre choix: ");
     scanf("%d",&choix) ;
     printf("\n\n\n") ;
@@ -155,8 +218,10 @@ void display()
                 DisplayPrintFile();
                 break;
             case 6:
-                DisplayChangeRegion();
+                DisplayCreateNewFile();
                 break;
+            case 7:
+                DisplayChangeRegion() ;
             default:
                 printf("Choix invalide. Veuillez saisir un nombre entre 0 et 6.\n");
                 break;
