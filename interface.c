96aa513c4658fa2f6099d2e497279af69bab1387
yacineallag19 <154988480+yacineallag19@users.noreply.github.com>
2024-01-01 18:15:55 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -9,6 +9,14 @@ printf("\t\t\t\t\033[1mBOUDRAA MAHDI\n");
 printf("\033[1mCe programme sert a manipuler une simple base de donnee de l'armee algerienne\n");
 printf("\033[1mIl y a un fichier deja cree -- si vous voulez tester notre options sur le\n");
 printf("\033[1mSi vous voulez tester sur un nouver fichier, vous pouvez choisir l'option de creer un nouveau fichier, suivre les etapes et puis choisir une option\n");
-printf("\033[1mMerci de choisir une option: ");
+printf("\033[1mMerci de choisir une option de ces options: \n\n\n");
+printf("\033[1m0. Quitter le programme");
+printf("\033[1m1. Recherche d'un soldat par matricule\n");
+printf("\033[1m2. Ajouter un soldat\n");
+printf("\033[1m3. Supprimer un solday depuis matricule\n");
+printf("\033[1m4. Requete intervalle\n");
+printf("\033[1m5. Afficher tous les soldats\n");
+printf("\033[1m6. Changer la region militaire d'un soldat utilisons son matricule\n");
+
 
 }
